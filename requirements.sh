710026e45c2c68ff5cccb4e46ca3c6ef2a6860c9
DamianOlei <100954177+DamianOlei@users.noreply.github.com>
2022-03-04 11:50:08 -0500

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+sudo apt install numpy
+sudo apt install scipy
+sudo apt install Pillow
+sudo apt install cython
+sudo apt install matplotlib
+sudo apt install scikit-image
+sudo apt install keras>=2.0.8
+sudo apt install opencv-python
+sudo apt install h5py
+sudo apt install imgaug
+sudo apt install IPython[all]
+
